2a24a572b29ee2356961f6cfda3899b4f9beaab1
francisco <programador2684@gmail.com>
2022-03-03 14:43:42 -0500

    commit cuatro

---
+++ CalculoSueldo/DoWhile/DoWhile/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer
+        x = 0
+
+        Do While x <= 10
+            Console.Write(x)
+            Console.Write("-")
+            x = x + 1
+        Loop
+        Console.ReadKey()
+    End Sub
+
+End Module
